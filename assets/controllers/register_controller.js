f661a9e16a6b9ff6aa9e4aa81a00ec7313a236f2
Nabil080 <bellilanabil8@gmail.com>
2023-12-27 16:36:50 +0100

    validator + step 0

--- assets/controllers/register_controller.js
+++ assets/controllers/register_controller.js
@@ -22,21 +22,51 @@ export default class extends Controller {
         })
     }
 
-    goToNextStep(e) {
+    handleNextButton(e) {
         e.preventDefault()
-
-        let isNotLastStep = this.currentStepValue < this.steps.length - 1
         let hasNoError = this.checkIfValid(this.currentStepValue)
 
-        console.log(isNotLastStep)
-        if (isNotLastStep && hasNoError) {
-            this.currentStepValue++
-            this.showStep(this.currentStepValue)
-        }
+        if (hasNoError) this.goToNextStep()
+    }
 
+    goToNextStep() {
+        this.currentStepValue++
+        this.showStep(this.currentStepValue)
     }
 
     checkIfValid(stepNumber) {
+        // Vérifie que c'est pas la dernière étape
+        let isLastStep = validator(checkIfLastStep, [this.currentStepValue, this.steps.length])
+        if (isLastStep) return false
+
+        // Vérifie qu'il n'y a pas d'erreur utilisateur
+        switch (stepNumber) {
+            case 0:
+                let email = this.steps[0].querySelector('input').value
+                let isEmailValid = validator(checkEmail, email)
+
+                if (isEmailValid) return this.sendValidationMail(email)
+
+
+                return false
+            default:
+                break;
+        }
         return true
     }
+
+    sendValidationMail(email) {
+        let todo = 'Envoie le mail de validation'
+
+
+        return todo ?? false
+    }
 }
+
+// validator
+const validator = (validator, value) => validator(value)
+
+const checkIfLastStep = value => value[0] === value[1] - 1 
+const checkEmail = value => (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/).test(value)
+
+
