bf8a741c3460942199e1b7d2a0fe7050dff48dfd
Nabil080 <bellilanabil8@gmail.com>
2024-01-02 20:18:22 +0100

    fix chart center

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,7 @@ import "./styles/app.css";
 import "./styles/date_picker.css";
 
 
+
 // stimulus
 import "./bootstrap.js";
 
--- assets/controllers/chart/pie_controller.js
+++ assets/controllers/chart/pie_controller.js
@@ -10,7 +10,7 @@ export default class extends Controller {
 
     connect() {
         let options = this.options
-        new ApexCharts(this.element, options).render()
+        new ApexCharts(this.divTarget, options).render()
     }
 
     get options() {
