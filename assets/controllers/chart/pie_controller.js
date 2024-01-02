6f694b454140913b60167855fcd682bdb2a6f8f6
Nabil080 <bellilanabil8@gmail.com>
2024-01-02 20:09:00 +0100

    data-value pour les données du graph

--- assets/controllers/chart/pie_controller.js
+++ assets/controllers/chart/pie_controller.js
@@ -3,22 +3,25 @@ import ApexCharts from "apexcharts";
 
 export default class extends Controller {
     static targets = ["div"];
+    static values = {
+        series: { type: Array, default: [80, 34, 45, 12] },
+        labels: { type: Array, default: ["Camping-car", "Caravane", "Port", "Emplacement libre"] }
+    }
 
     connect() {
         let options = this.options
-
-        new ApexCharts(this.element,options).render()
+        new ApexCharts(this.element, options).render()
     }
 
     get options() {
         return {
-            series: [80, 34, 45, 12],
+            series: this.seriesValue,
             chart: {
                 type: "donut",
                 width: 450,
             },
             colors: ["#3C50E0", "#6577F3", "#8FD0EF", "#0FADCF"],
-            labels: ["Camping-car", "Caravane", "Port", "Emplacement libre"],
+            labels: this.labelsValue.map(string => string.replace("_"," ")),
             legend: {
                 show: true,
                 position: "bottom",
@@ -31,7 +34,6 @@ export default class extends Controller {
 
             plotOptions: {
                 pie: {
-                    // size: 450,
                     donut: {
                         size: "65%",
                         background: "transparent",
