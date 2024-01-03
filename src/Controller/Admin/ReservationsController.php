7893812ded48ec6a51dce4b271a5f0da6a449d98
Nabil080 <bellilanabil8@gmail.com>
2024-01-03 12:28:06 +0100

    fix sidebar page = route name + reservation controller

---
+++ src/Controller/Admin/ReservationsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/reservations', name: 'app_admin_reservations')]
+class ReservationsController extends AbstractController
+{
+    private function getPath($file): string
+    {
+        return sprintf('admin/reservations/%s.html.twig', $file);
+    }
+
+    #[Route('/pointages', name: '_day')]
+    public function index(): Response
+    {
+        return $this->render($this->getPath("day"), [
+            'controller_name' => 'ReservationsController',
+        ]);
+    }
+}
