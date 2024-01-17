f4f02e5095c7c8e306dac892642ef276b3f7c8e7
Nabil080 <bellilanabil8@gmail.com>
2024-01-17 17:16:20 +0100

    settings controller + base et index

---
+++ src/Controller/Admin/Settings/SettingsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin\Settings;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/settings', name: 'app_admin_settings')]
+class SettingsController extends AbstractController
+{
+    private function getPath($file): string
+    {
+        return sprintf('admin/settings/%s.html.twig', $file);
+    }
+
+    #[Route('/', name: '')]
+    public function index(): Response
+    {
+        return $this->render($this->getPath('index'), [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
