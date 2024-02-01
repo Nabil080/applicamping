320d81361c9d9037fecf0569c4f7af1d645d6a36
Nabil080 <bellilanabil8@gmail.com>
2024-02-01 18:52:04 +0100

    option supp add update

--- src/Form/OptionType.php
+++ src/Form/OptionType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Option;
+use App\Form\Type\CustomStatutType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,7 +14,7 @@ class OptionType extends AbstractType
     {
         $builder
             ->add('nom')
-            ->add('statut')
+            ->add('statut', CustomStatutType::class)
         ;
     }
 
